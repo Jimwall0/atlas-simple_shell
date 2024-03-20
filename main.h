bb392a9ea4394b20968483da386e6b3a34a47f19
Tsimmuaj <tyang4204@gmail.com>
2024-03-20 13:38:30 -0500

    updated head

--- main.h
+++ main.h
@@ -2,6 +2,9 @@
 #define MAIN_H
 #define EXIT_SUCCESS 0
 #define EXIT_FAILURE 1
+#define ERROR_MALLOC 77
+#define EXIT_F_STAT 23
+#define EXIT_F_ACCESS 24
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
