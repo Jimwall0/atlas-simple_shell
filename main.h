61fe38c32923d30aac37a0c3c8f80771a818e79d
Tsimmuaj <tyang4204@gmail.com>
2024-03-20 13:36:13 -0500

    updated macro

--- main.h
+++ main.h
@@ -1,9 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
+#define EXIT_SUCCESS 0
+#define EXIT_FAILURE 1
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 int main(void);
+int _strcmp(char *string1, char *string2);
 
 #endif
