f1aaa4ee134dca74a141b048ae9c4463f2746b29
LRWyatt801 <loganwyatt1995@gmail.com>
2024-03-20 19:32:04 -0500

    fork + wait + execve funtional

--- main.c
+++ main.c
@@ -21,12 +21,33 @@ int main(int argc, char **argv, char **envp)
 	(void)envp;
 	char *buffer = NULL, *tok = NULL;
 	size_t size = BUFF_SIZE;
+	pid_t pid;
 
 	buffer = malloc(sizeof(char) * size);
 	if (buffer == NULL)
 		return (ERROR_MALLOC);
 	tok = user_input(buffer, size);
+
+	pid = fork();
+	if (pid < 0)
+		return (EXIT_FAILURE);
+	else if (pid == 0)
+	{
+		if (execve(tok, argv, NULL) == -1)
+		{
+			free(buffer);
+			free(tok);
+			exit(EXIT_FAILURE);
+		}
+		exit(EXIT_SUCCESS);
+	}
+	else
+	{
+		wait(NULL);
+	}
+
 	printf("%s\n", tok);
 	free(buffer);
+	free(tok);
 	return (EXIT_SUCCESS);
 }
