47984a8bcff911353a4698e9c976ce7012f09de5
LRWyatt801 <loganwyatt1995@gmail.com>
2024-03-29 18:32:14 -0500

    Consolidate main.c.  Changed isatty to close loop rather than else if.  Also fixed non-interactive mode to handle PATH

--- main.c
+++ main.c
@@ -10,66 +10,45 @@ int main(int ac, char **av, char **env)
 {
 	char **cmd_arr = NULL;
 	path_t *cmd_list = NULL;
-	int check = 0;
+	int check = 1;
 	pid_t pid;
 
 	(void)ac;
 	(void)av;
-	(void)env;
-
 	pathlist(&cmd_list); /*get list of PATH*/
-	if (isatty(STDIN_FILENO))
+	while (check)
 	{
-		while (check == 0)
-		{
-			printf("%s ", PROMPT);
-			cmd_arr = user_input();
-			cmd_arr[0] = pathfinder(cmd_list, cmd_arr[0]);
-			if (cmd_arr[0] == NULL)
-			{
-				free2darray(cmd_arr);
-				continue;
-			}
-			else if (strcmp(cmd_arr[0], "exit") == 0)
-			{
-				check++;
-				free2darray(cmd_arr);
-				freelist(&cmd_list);
-				return (EXIT_SUCCESS);
-			}
-
-			pid = fork();
-			if (pid == 0)
-			{
-				while (cmd_arr)
-				{
-					if (execve(cmd_arr[0], cmd_arr, env) != 0)
-					{
-						free2darray(cmd_arr);
-						exit(EXIT_FAILURE);
-					}
-				}
-				exit(EXIT_SUCCESS);
-			}
-			wait(NULL);
-			free2darray(cmd_arr);
-		}
-	}
-	else
-	{
+		printf("%s ", PROMPT);
 		cmd_arr = user_input();
-		pid = fork();
+		cmd_arr[0] = pathfinder(cmd_list, cmd_arr[0]);
+		if (!isatty(STDIN_FILENO))
+			check = 0;
+		if (cmd_arr[0] == NULL)/*for cmd not found*/
+		{
+			free2darray(cmd_arr);
+			continue;
+		}
+		else if (strcmp(cmd_arr[0], "exit") == 0)/*exit*/
+		{
+			free2darray(cmd_arr);
+			freelist(&cmd_list);
+			return (EXIT_SUCCESS);
+		}
+		pid = fork();/*fork for child proccess*/
 		if (pid == 0)
 		{
-			if (execve(cmd_arr[0], cmd_arr, env) != 0)
+			while (cmd_arr)
 			{
-				free2darray(cmd_arr);
-				exit(EXIT_FAILURE);
+				if (execve(cmd_arr[0], cmd_arr, env) != 0)
+				{
+					free2darray(cmd_arr);
+					exit(EXIT_FAILURE);
+				}
 			}
+			exit(EXIT_SUCCESS);
 		}
 		wait(NULL);
+		free2darray(cmd_arr);
 	}
-	freelist(&cmd_list);
-	free2darray(cmd_arr);
-	return (0);
+	return (EXIT_SUCCESS);
 }
